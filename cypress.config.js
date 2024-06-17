0ccf3f41450cb4f7ccb81c9ee4459c251248f3c5
Guilherme <willfkrause@outlook.com>
2024-06-17 13:44:25 -0300

    test-grep

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,18 @@
 const { defineConfig } = require("cypress");
+const grep = require("@cypress/grep/src/plugin");
 
 module.exports = defineConfig({
   projectId: "6sbhfb",
   e2e: {
     setupNodeEvents(on, config) {
+      grep(config)
       // implement node event listeners here
     },
+  },
+  component: {
+    setupNodeEvents(on, config) {},
+  },
+  env: {
+    grepFilterSpecs: true,
   },
 });
---
+++ cypress/e2e/teste.cy.js
@@ -0,0 +1,5 @@
+describe('template spec',{ tags: ["@teste"]}, () => {
+    it('passes', () => {
+      cy.visit('https://example.cypress.io')
+    })
+  })
