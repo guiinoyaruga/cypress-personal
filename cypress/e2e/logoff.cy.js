95657804a3127ddcf47828f5d71b306b78e6477b
Guilherme <willfkrause@outlook.com>
2024-06-20 18:02:16 -0300

    teste

--- cypress/e2e/logoff.cy.js
+++ cypress/e2e/logoff.cy.js
@@ -1,4 +1,4 @@
-describe('logoff test',{ tags: ["@teste"]}, () => {
+describe('logoff test',{ tags: ["@teste1"]}, () => {
     it('do logoff', () => {
       cy.visit('https://example.cypress.io')
     })
